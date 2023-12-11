49b440d7083a756b93c6169dfede62b0d790939c
ALEXANDRE DA SILVA SOUZA <alexandresouza9@gmail.com>
2023-12-11 18:44:49 -0300

    commit
    iniciando

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá mundo");
+            
+        }
+    }
+}
